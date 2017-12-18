1f569df2c1967ae90a9114431e7a99f2398a900b
Memnoc <smartdroidesign@gmail.com>
2017-12-18 23:06:55 +0000

    Added onClickListener to all the buttons in CalcActivity.java Added two surfaceView for the image buttons.

--- app/src/main/java/com/smartdroidesign/materialcalculator/CalcActivity.java
+++ app/src/main/java/com/smartdroidesign/materialcalculator/CalcActivity.java
@@ -1,7 +1,12 @@
 package com.smartdroidesign.materialcalculator;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class CalcActivity extends Activity {
 
@@ -9,5 +14,114 @@ public class CalcActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        Button oneButton = (Button)findViewById(R.id.oneButton);
+        Button twoButton = (Button)findViewById(R.id.twoButton);
+        Button threeButton = (Button)findViewById(R.id.threeButton);
+        Button fourButton = (Button)findViewById(R.id.fourButton);
+        Button fiveButton = (Button)findViewById(R.id.fiveButton);
+        Button sixButton = (Button)findViewById(R.id.sixButton);
+        Button sevenButton = (Button)findViewById(R.id.sevenButton);
+        Button eightButton = (Button)findViewById(R.id.eightButton);
+        Button nineButton = (Button)findViewById(R.id.nineButton);
+        Button zeroButton = (Button)findViewById(R.id.zeroButton);
+        Button dotButton = (Button)findViewById(R.id.dotButton);
+
+
+        ImageButton equalButton = (ImageButton) findViewById(R.id.equalButton);
+        ImageButton divideButton = (ImageButton)findViewById(R.id.divideButton);
+        ImageButton multiplyButton = (ImageButton)findViewById(R.id.multiplyButton);
+        ImageButton subtractButton = (ImageButton)findViewById(R.id.subtractButton);
+        ImageButton addButton = (ImageButton)findViewById(R.id.addButton);
+
+        Button delButton = (Button)findViewById(R.id.delButton);
+        TextView resultsText = (TextView)findViewById(R.id.resultsText);
+
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        twoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        threeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sixButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sevenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        eightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        nineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        zeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        dotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        delButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
